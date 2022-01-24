b545096328cf9d79285b004ac702d02f9947a501
“erikasnts22” <sntserika22@gmail.com>
2022-01-24 21:22:39 +0800

    config db and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get 'welcome/index'
+
+
+  resources :docs
+  authenticated :user do
+    root 'welcome#index', as: 'authenticated_root'
+  end
+
+  root 'welcome#index'
+
+  delete 'docs/:id/delete' => 'docs#destroy', as: 'docs_delete'
+  get '/docs/:id/delete' => 'docs#destroy'
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
